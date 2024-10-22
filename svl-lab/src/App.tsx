d9bf9fa7e412d619647e7119f539eca6c818f376
Utkarsh Singh <singh.utkarsh26@gmail.com>
2024-10-22 00:46:36 -0700

    text on the vortex

--- svl-lab/src/App.tsx
+++ svl-lab/src/App.tsx
@@ -1,15 +1,28 @@
-// import { LampDemo } from '../src/components/ui/lamp';
 import { Vortex } from '../src/components/ui/vortex';
-// import { VortexDemo } from '../src/components/VortexDemo';
+
 
 
 function App() {
 
   return (
     <>
-      {/* <LampDemo></LampDemo> */}
-      <Vortex></Vortex>
-      {/* <VortexDemo></VortexDemo> */}
+
+      <Vortex backgroundColor="black"
+        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
+      <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+          Welcome to SVL at ASU
+        </h2>
+        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
+        We research data visualization, human-computer interaction, and advanced interfaces for data science,
+        under the direction of <a href="https://chrisbryan.github.io/" target='_blank'>Professor Chris Bryan.</a>
+        </p>
+        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
+        <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
+            Find out more about us
+          </button>
+          </div>
+      </Vortex>
+
 
     </>
   )
