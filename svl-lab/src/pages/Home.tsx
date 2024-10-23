0a74ff4b81b0067806bf66af649ab26b8a923dcb
Utkarsh Singh <singh.utkarsh26@gmail.com>
2024-10-22 17:03:27 -0700

    second vortex (testing it out) for home instead of first

--- svl-lab/src/pages/Home.tsx
+++ svl-lab/src/pages/Home.tsx
@@ -1,14 +1,14 @@
-import { Vortex } from '../components/ui/vortex';
+// import { Vortex } from '../components/ui/vortex';
 import { NavbarDemo } from '../components/NavbarDemo';
+import { VortexDemoSecond } from '../components/VortexDemoSecond'
 
 function Home() {
 
   return (
     <>
-      <Vortex backgroundColor="black"
-        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
+      <VortexDemoSecond>
             <NavbarDemo></NavbarDemo>
-      </Vortex>
+      </VortexDemoSecond>
     </>
   );
 }
